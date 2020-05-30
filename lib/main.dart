e1da470dc9f498106250ebd016a40890b58c9f42
Siddhant356 <siddhantshukla356@gmail.com>
2020-05-31 04:31:27 +0530

    Login Page design update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:loginpagedesign/Animation/FadeAnimation.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
@@ -23,11 +24,12 @@ class _HomePageState extends State<HomePage> {
               child: Stack(
                 children: <Widget>[
                   Positioned(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("assets/images/Path3.png"),
+                    child: FadeAnimation(1, Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/images/Path3.png"),
+                          ),
                         ),
                       ),
                     ),
@@ -42,99 +44,111 @@ class _HomePageState extends State<HomePage> {
               padding: EdgeInsets.symmetric(
                 horizontal: 20.0,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Hello there, \nWelcome back",
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.transparent,
-                    ),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.all(10.0),
-                          decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                              color: Colors.grey[100],
-                            )),
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Username",
-                              hintStyle: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.all(10.0),
-                          decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(
-                              color: Colors.grey[100],
-                            )),
-                          ),
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Password",
-                              hintStyle: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Center(
-                    child: Text(
-                      "Forgot Password?",
+              child: FadeAnimation(1, Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Hello there, \nWelcome back",
                       style: TextStyle(
-                        color: Colors.pink[200],
+                        fontSize: 30.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    height: 50.0,
-                    margin: EdgeInsets.symmetric(horizontal: 60),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Color.fromRGBO(49, 39, 79, 1)),
-                    child: Center(
-                      child: Text(
-                        "Login",
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                FadeAnimation(1, Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.transparent,
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                color: Colors.grey[100],
+                              )),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Username",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(
+                                color: Colors.grey[100],
+                              )),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Password",
+                                hintStyle: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Center(
+                      child: FadeAnimation(1, Text(
+                        "Forgot Password?",
                         style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.pink[200],
+                        ),
+                      ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                FadeAnimation(1, Container(
+                      height: 50.0,
+                      margin: EdgeInsets.symmetric(horizontal: 60),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Color.fromRGBO(49, 39, 79, 1)),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20.0,),
-                  Center()
-                ],
+                ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Center(
+                      child: FadeAnimation(1, Text(
+                        "Create Account",
+                        style: TextStyle(color: Colors.pink[200]),
+                      ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
